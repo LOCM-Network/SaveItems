faf74d2bb148e2ae778c884b1694d66dabb97192
furyZ <52622140+Fuzuto@users.noreply.github.com>
2024-11-21 18:31:27 -0500

    Modified to Inventory menu.

--- src/SaveItems/main.php
+++ src/SaveItems/main.php
@@ -2,28 +2,28 @@
 
 namespace SaveItems;
 
+use muqsit\invmenu\InvMenu;
+use muqsit\invmenu\InvMenuHandler;
+use muqsit\invmenu\type\InvMenuTypeIds;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\event\Listener;
+use pocketmine\inventory\Inventory;
 use pocketmine\item\Item;
-use pocketmine\item\VanillaItems;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 
-class main extends PluginBase implements Listener{
-	private static ?main $instance = null;
+class main extends PluginBase {
+
 	private Config $data;
 
-	public function onEnable() : void{
-		@mkdir($this->getDataFolder());
-		self::$instance = $this;
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->data = new Config($this->getDataFolder() . "data.yml", Config::YAML);
-	}
+	protected function onEnable(): void {
+		if (!InvMenuHandler::isRegistered()) {
+			InvMenuHandler::register($this);
+		}
 
-	public static function getInstance() : ?main{
-		return self::$instance;
+		@mkdir($this->getDataFolder());
+		$this->data = new Config($this->getDataFolder() . "data.yml", Config::YAML);
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
@@ -31,89 +31,45 @@ class main extends PluginBase implements Listener{
 			$sender->sendMessage("This command can only be used in-game.");
 			return false;
 		}
-		switch ($command->getName()) {
-			case "saveitems":
-				$this->saveItem($sender);
-				return true;
-			case "takeitems":
-				$this->getItem($sender);
-				return true;
+
+		if ($command->getName() === "savesitem") {
+			$this->openSavedItemsMenu($sender);
+			return true;
 		}
+
 		return false;
 	}
 
-	private function saveItem(Player $player): void {
-		$inventory = $player->getInventory();
-		$items = $inventory->getContents();
+	private function openSavedItemsMenu(Player $player): void {
+		$menu = InvMenu::create(InvMenuTypeIds::TYPE_CHEST)
+			->setName("Saved Items")
+			->setInventoryCloseListener(function (Player $player, Inventory $inventory): void {
+				$this->saveInventoryItems($player, $inventory);
+			});
 
-		if (empty($items)) {
-			$player->sendMessage("Your inventory is empty.");
-			return;
-		}
-
-		$playerName = $player->getName();
-		$savedItems = [];
-
-		foreach ($items as $slot => $item) {
-			if (!$item->isNull()) {
-				$savedItems[] = self::encodeItem($item);
-				$inventory->setItem($slot, VanillaItems::air()); // Clear the item
-			}
-		}
-
-		$this->data->set($playerName, $savedItems);
-		$this->data->save();
-
-		$player->sendMessage("All items in your inventory have been saved!");
+		$this->loadSavedItems($player, $menu->getInventory());
+		$menu->send($player);
 	}
 
-	private function getItem(Player $player): void {
-		$playerName = $player->getName();
-
-		if (!$this->data->exists($playerName) || empty($this->data->get($playerName))) {
-			$player->sendMessage("You have no saved items.");
-			return;
-		}
-
-		$savedItems = array_map([self::class, 'decodeItem'], $this->data->get($playerName));
-		$inventory = $player->getInventory();
-
-		foreach ($savedItems as $item) {
-			if (!$inventory->canAddItem($item)) {
-				$player->sendMessage("Not enough space in your inventory to retrieve saved items!");
-				return;
-			}
-		}
-
-		foreach ($savedItems as $item) {
+	private function loadSavedItems(Player $player, Inventory $inventory): void {
+		$savedItems = $this->data->get($player->getName(), []);
+		foreach (array_map([$this, "decodeItem"], $savedItems) as $item) {
 			$inventory->addItem($item);
 		}
+	}
 
-		$this->data->remove($playerName);
+	private function saveInventoryItems(Player $player, Inventory $inventory): void {
+		$items = array_map([$this, "encodeItem"], array_filter($inventory->getContents(), fn(Item $item) => !$item->isNull()));
+		$this->data->set($player->getName(), $items);
 		$this->data->save();
-
-		$player->sendMessage("All saved items have been added back to your inventory!");
+		$player->sendMessage("Your items have been saved!");
 	}
 
-
-	public static function encodeItem(Item $item) : string {
-		$itemToJson = self::itemToJson($item);
-		return base64_encode(gzcompress($itemToJson));
+	private function encodeItem(Item $item): string {
+		return base64_encode(gzcompress(serialize($item->nbtSerialize())));
 	}
 
-	public static function decodeItem(string $item) : Item {
-		$itemFromJson = gzuncompress(base64_decode($item));
-		return self::jsonToItem($itemFromJson);
-	}
-
-	public static function itemToJson(Item $item) : string {
-		$cloneItem = clone $item;
-		$itemNBT = $cloneItem->nbtSerialize();
-		return base64_encode(serialize($itemNBT));
-	}
-
-	public static function jsonToItem(string $json) : Item {
-		$itemNBT = unserialize(base64_decode($json));
-		return Item::nbtDeserialize($itemNBT);
+	private function decodeItem(string $data): Item {
+		return Item::nbtDeserialize(unserialize(gzuncompress(base64_decode($data))));
 	}
 }
